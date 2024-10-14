00643dae194d6a43f7274bc5d6e921b9a824a009
Jethro Jens Norbert Simatupang <19623063@std.stei.itb.ac.id>
2024-10-14 21:51:46 +0700

    a

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import lib.Matrix;
+import lib.SPL;
+import lib.determinan;
+import lib.invers;
+import lib.InterpolasiPolinomial;
+import lib.RegresiBerganda;
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class main {
+    public static void main(String[] args) {
+        RegresiBerganda.RegresiLinierBerganda(args);
+    }
+}
